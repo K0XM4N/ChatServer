167b08cb4b340d89e08646cc2ab14c36b10940cc
K0XM4N <radwan.krzysztof.1993@gmail.com>
2017-01-25 14:56:22 +0100

    Initial commit. Added project to GitHub

---
+++ src/controller/ServerController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class ServerController {
+    public void handleStartServer(ActionEvent event) {
+
+    }
+
+    public void handleStopServer(ActionEvent event) {
+
+    }
+}
